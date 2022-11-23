3ec138bf15daf0f4ce1c9e55f92d422890016c1e
icaronunes22 <109877001+icaronunes22@users.noreply.github.com>
2022-11-23 16:16:19 -0300

    atual

--- Java.js
+++ Java.js
@@ -8,32 +8,34 @@
 		$("#cpf").mask("999.999.999-99");
 	});
 </script>*/
-const btn = document.querySelector("#salario");
+/*const btn = document.querySelector("#salario");
 btn.addEventListener("click", function(e) {
     e.preventDeFault();
     const salario = document.querySelector("salario")
     const value = quantidade.value
 console.log(value);
 
-});
-salarioBasico = 1212 
-salarioEfetivo = 1450
-salarioChefeDeEquipe = 2312
-salarioSubGerente = 3500
-salarioGerente = 5320
-salarioDiretor = 7580
-salarioPresidente = 11300
+});*/
 
-cargoAprendiz = salarioBasico
-maximo = 50%
-nivelUm > 20%
-nivelDois > 30%
-nivelTres > 40%
-nivelQuatro > 50%
-nivelQuinto = maximo
+let salarioBasico = 1212 ;
+let salarioEfetivo = 1450;
+let salarioChefeDeEquipe = 2312;
+let salarioSubGerente = 3500;
+let salarioGerente = 5320;
+let salarioDiretor = 7580;
+let salarioPresidente = 11300;
 
-SalariosPadroes []
+cargoAprendiz = salarioBasico;
+basico = 0%
+nivelUm = basico
+nivelDois = 20%
+nivelTres = 30%
+nivelQuatro = 40%
+nivelQuinto = 50
 
+SalariosPadroes [salarioBasico, SalarioEfetivo, salarioChefeDeEquipe, salarioSubGerente, salarioGerente, salarioDiretor, salarioPresidente];
+
+console.log(SalariosPadroes)
 /*let valor = salarioTotal;
 let texto = valor.toLocaleString("pt-BR", 
      { style: "currency" , currency:"BRL"});
